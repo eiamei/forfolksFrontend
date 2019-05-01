d62cfa755e010044fc34a3195f8f8ecced1b7ff6
eiamei <maximambrosevich@gmail.com>
2019-05-01 11:33:28 +0300

    make about page v 1.0

---
+++ src/Components/Pages/About/About.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="about">
+    <h1 class="about__header">Контакты</h1>
+    <div class="about__contacts">
+      <p>hello.forfolks@gmail.com</p>
+      <p>+7 (999) 239-66-10</p>
+      <span>
+        <a href="https://vk.com/weareforfolks" target="_blank">
+          <div class="about__vk"></div>
+        </a>
+        <a href="https://instagram.com/weareforfolks" target="_blank">
+          <div class="about__instagram"></div>
+        </a>
+        <a href="https://facebook.com/weareforfolks" target="_blank">
+          <div class="about__facebook"></div>
+        </a>
+      </span>
+      <h5>Интернет магазин</h5>
+      <p class="about__address"> Санкт-Петербург, Средний проспект В.О. 56</p>
+      <h5>Партнеры</h5>
+      <span>
+        <p class="about__store">Малина</p>
+        <p class="about__address"> Санкт-Петербург, Лиговский проспект 54, Лофт проект этажи, этаж 1.2</p>
+        <p class="about__store">More flowers</p>
+        <p class="about__address"> Санкт-Петербург, Большая Разночинная 16, 1 этаж, секция 119</p>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: ''
+  };
+</script>
+
+<style lang="scss">
+  .about {
+    text-align: center;
+    margin-top: 20vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    &__header {
+      font-weight: 300;
+    }
+    &__instagram, &__vk, &__facebook {
+      display: inline-block;
+      width: 30px;
+      height: 30px;
+      margin: 0 8px;
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+    &__instagram {
+      background: url('../../../assets/svg/instagram-logo.svg') no-repeat;
+    }
+    &__vk {
+      background: url('../../../assets/svg/vk-logo.svg') no-repeat;
+    }
+    &__facebook {
+      background: url('../../../assets/svg/facebook-logo.svg') no-repeat;
+    }
+    &__store {
+      margin: 16px 0 0 0;
+    }
+    &__address {
+      font-size: 12px;
+      margin: 8px 0 0 0;
+    }
+  }
+</style>
