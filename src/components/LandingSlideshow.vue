b8451abb97ca8dda2f69c6dd9442dfa3d2b96dd9
eiamei <maximambrosevich@gmail.com>
2020-02-21 23:08:08 +0300

    Added extra description for the routes and for the landing.

--- src/components/LandingSlideshow.vue
+++ src/components/LandingSlideshow.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="landing-slide-show">
-    <img class="landing-slide-show__image" src="../assets/images/store/bowl-mild-black-marble.jpg">
+    <img class="landing-slide-show__image" src="../assets/images/store/landing-slideshow.jpg" alt="hero image">
   </div>
 </template>
 
@@ -13,10 +13,14 @@ export default {
 <style lang="scss">
   .landing-slide-show {
     margin: 3rem 1rem 0 1rem;
+    display: flex;
+    justify-content: center;
     &__image {
-      height: calc(100vh - 10rem);
+      max-width: 1200px;
+      height: calc(100vh - 20rem);
       width: 100%;
       object-fit: cover;
+      object-position: 100% 100%;
     }
   }
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="landing-page">
     <landing-slideshow/>
-    <section class="landing-page-heading">
+    <article class="landing-page-heading">
       <h1>Дизайн для современной жизни</h1>
-      <p class="landing-page-heading__hero-text">Мы - молодая студия из Питера. Интерьер и декор наша страсть, которой мы хотим заразить всех вокруг. Настало время отбросить стереотипы и сделать свой дом в стиле лучших постов из Instagram и Pinterest.</p>
-    </section>
+      <p class="landing-page-heading__hero-text">{{heroText}}</p>
+    </article>
     <article class="landing-page-about">
       <section class="landing-page-about__container" v-for="(item, index) in about" :key="item.header">
         <img :class="aboutImageClass(index)" :src="item.image" :alt="item.header"/>
@@ -26,22 +26,26 @@ export default {
   components: {LandingSlideshow},
   data () {
     return {
+      heroText: 'Forfolks - семейная студия, создающая товары для придания особой атмосферы в вашем доме или офисе.\n' +
+        'Наши изделия - это сочетание лаконичности, скандинавского минимализма и демократичных цен.\n' +
+        'Наша политика - декор должен быть доступный, чтобы каждый смог обустроить свой дом. \n' +
+        'Надеемся, что мы сможем сделать ваш дом немножко красивее!\n',
       about: [{
-        image: require("../assets/images/store/landing_concrete.jpg"),
+        image: require("../assets/images/store/landing-concrete.jpg"),
         header: 'Бетон',
-        text: 'Мы - молодая студия из Питера. Интерьер и декор наша страсть, которой мы хотим заразить всех вокруг. Настало время отбросить стереотипы и сделать свой дом в стиле лучших постов из Instagram и Pinterest.'
+        text: 'Прочный и эффектный. Столь привычный строительный материал сегодня переживает свой ренесанс в ' +
+          'совершенно не свойственной, но такой подходящей для него среде. Он способен принять практически любую форму ' +
+          'и привнести особый шарм в интерьер.'
       }, {
-        image: require("../assets/images/store/landing_concrete.jpg"),
-        header: 'Бетон',
-        text: 'Мы - молодая студия из Питера. Интерьер и декор наша страсть, которой мы хотим заразить всех вокруг. Настало время отбросить стереотипы и сделать свой дом в стиле лучших постов из Instagram и Pinterest.'
+        image: require("../assets/images/store/landing-dryflower.jpg"),
+        header: 'Сухоцветы',
+        text: 'Красивые на вечность. Мы любим сухоцветы за безграничную нежность их оттенков и безприкословную неприхотливость. ' +
+          'Это кусочек дикой природы, навсегда застывший в своем самом прекрасном состоянии.'
       },{
-        image: require("../assets/images/store/landing_concrete.jpg"),
-        header: 'Бетон',
-        text: 'Мы - молодая студия из Питера. Интерьер и декор наша страсть, которой мы хотим заразить всех вокруг. Настало время отбросить стереотипы и сделать свой дом в стиле лучших постов из Instagram и Pinterest.'
-      }, {
-        image: require("../assets/images/store/landing_concrete.jpg"),
-        header: 'Бетон',
-        text: 'Мы - молодая студия из Питера. Интерьер и декор наша страсть, которой мы хотим заразить всех вокруг. Настало время отбросить стереотипы и сделать свой дом в стиле лучших постов из Instagram и Pinterest.'
+        image: require("../assets/images/store/landing-cotton.jpg"),
+        header: 'Хлопок',
+        text: 'Нежный и натуральный. Удивительную легкость в интерьер могут привнести изделия из хлопка, наполняя его ' +
+          'уютом и теплом. В своих изделях мы используем 100% хлопок.'
       }]
     }
   },
@@ -49,13 +53,13 @@ export default {
     aboutImageClass (index) {
       return {
         'landing-page-about__image': true,
-        'landing-page-about__image--right': !!((index + 1) % 2)
+        'landing-page-about__image--right': Boolean((index + 1) % 2)
       }
     },
     aboutDescriptionClass (index) {
       return {
         'landing-page-about__description': true,
-        'landing-page-about__description--right': !((index + 1) % 2)
+        'landing-page-about__description--right': Boolean(!((index + 1) % 2))
       }
     }
   }
@@ -72,26 +76,29 @@ export default {
     align-items: center;
     padding: 0 1rem;
     &__hero-text {
-      max-width: 800px;
+      max-width: 900px;
       margin: 1rem;
     }
   }
   .landing-page-about {
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     align-items: center;
     margin-top: 3rem;
     &__container {
       display: flex;
-      max-width: 1200px;
+      max-width: 1000px;
       margin: 0 1rem 4rem 1rem;
     }
     &__image {
-      width: calc(50% - 3rem);
+      width: calc(50% - 2rem);
+      height: calc((50% - 2rem) / 2 * 3);
       margin: 0;
       order: 1;
+      object-fit: contain;
       &--right {
-        margin-right: 3rem;
+        margin-right: 4rem;
       }
     }
     &__description {
@@ -104,7 +111,7 @@ export default {
       font-size: 1rem;
       &--right {
         order: 0;
-        margin: 0 2rem 0 1rem;
+        margin: 0 4rem 0 1rem;
       }
     }
   }
