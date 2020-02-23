fa78d48a24f146d988ae6da0c665a349b7b4cf76
eiamei <maximambrosevich@gmail.com>
2020-02-23 22:09:45 +0300

    Added fade animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="app">
     <AppHeader/>
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -33,4 +35,15 @@ export default {
 
 <style lang="scss">
   @import "assets/styles/globals";
+  .fade-enter-active,
+  .fade-leave-active {
+    transition-duration: 0.3s;
+    transition-property: opacity;
+    transition-timing-function: ease;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0
+  }
 </style>
