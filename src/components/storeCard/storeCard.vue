00f1699e21f6ecbc90340fe903929d5a263b4e55
eiamei <maximambrosevich@gmail.com>
2020-06-09 19:19:59 +0300

    added colors picker to the catalogue

--- src/components/storeCard/storeCard.vue
+++ src/components/storeCard/storeCard.vue
@@ -1,9 +1,11 @@
 <template>
   <li class="store-card" :class="{'store-card--wide': isWide}">
-    <router-link class="store-card__image-link" :to="getLink()">
-      <img ref="image" :src="imageLink" loading="lazy" @load="imageLoaded"/>
+    <div class="store-card__image-link">
+      <router-link :to="getLink()">
+        <img class="store-card__image" ref="image" :src="imageLink" loading="lazy" @load="imageLoaded"/>
+      </router-link>
       <button class="regular-sans-text store-card__add-to-bag" @click="addToBag">Добавить в корзину</button>
-    </router-link>
+    </div>
     <div v-show="isShowInfo" class="store-card__text-wrapper">
       <router-link class="store-card__text-link" :to="getLink()">
         <p class="small-regular-heading store-card__model">{{item.name}}</p>
@@ -11,67 +13,67 @@
       </router-link>
       <p class="regular-sans-text store-card__price">{{item.price}}P</p>
     </div>
-<!--    <div v-show="isShowInfo">-->
-<!--      <button class="clean-button store-card__color-button" @click="addToBag"></button>-->
-<!--    </div>-->
+    <color-picker :item="item"/>
   </li>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-    import slugify from "slugify";
-    import { ItemInterface } from "../../services/Store/Shop/shop.types";
+  import Vue from 'vue';
+  import slugify from "slugify";
+  import {ItemInterface} from "../../services/Store/Shop/shop.types";
+  import ColorPicker from "../ColorPicker.vue";
 
-    interface Data {
-        isShowInfo: boolean;
-        isWide: boolean;
+  interface Data {
+    isShowInfo: boolean;
+    isWide: boolean;
+  }
+
+  export default Vue.extend({
+    name: 'StoreCard',
+    components: { ColorPicker },
+    props: {
+      item: {
+        type: Object as () => ItemInterface,
+        required: true
+      }
+    },
+    data(): Data {
+      return {
+        isShowInfo: false,
+        isWide: false
+      }
+    },
+    computed: {
+      // TODO compute it in the store
+      imageLink(): void {
+        let path = this.item.rootPath;
+        if (this.item.selectableProperty.length)
+          this.item.selectableProperty.forEach(property => {
+            path += `-${slugify(property.value.toLowerCase())}`
+          });
+        return require(`@/assets/images/store/${path}-small.jpg`);
+      }
+    },
+    methods: {
+      imageLoaded: function (): void {
+        this.isShowInfo = true;
+        // @ts-ignore
+        this.isWide = this.$refs.image.naturalWidth > this.$refs.image.naturalHeight;
+      },
+      getLink(): string {
+        let path = this.item.rootPath;
+        if (this.item.selectableProperty.length)
+          this.item.selectableProperty.forEach(property => {
+            path += `/${slugify(property.value.toLowerCase())}`
+          });
+        return `/product/${path}`;
+      },
+      addToBag(event): void {
+        event.preventDefault();
+        this.$store.dispatch('bag/add', {item: this.item});
+      }
     }
-
-    export default Vue.extend({
-        name: 'StoreCard',
-        props: {
-            item: {
-                type: Object as () => ItemInterface,
-                required: true
-            }
-        },
-        data(): Data {
-            return {
-                isShowInfo: false,
-                isWide: false
-            }
-        },
-        computed: {
-            // TODO compute it in the store
-            imageLink (): void {
-                let path = this.item.rootPath;
-                if (this.item.selectableProperty.length)
-                    this.item.selectableProperty.forEach(property => {
-                        path += `-${slugify(property.value.toLowerCase())}`
-                    });
-                return require(`@/assets/images/store/${path}-small.jpg`);
-            }
-        },
-        methods: {
-            imageLoaded: function (): void {
-                this.isShowInfo = true;
-                // @ts-ignore
-                this.isWide = this.$refs.image.naturalWidth > this.$refs.image.naturalHeight;
-            },
-            getLink (): string {
-                let path = this.item.rootPath;
-                if (this.item.selectableProperty.length)
-                    this.item.selectableProperty.forEach(property => {
-                        path += `/${slugify(property.value.toLowerCase())}`
-                    });
-                return `/product/${path}`;
-            },
-            addToBag (event): void {
-                event.preventDefault();
-                this.$store.dispatch('bag/add', { item: this.item });
-            }
-        }
-    });
+  });
 </script>
 
 <style lang="scss">
@@ -79,20 +81,20 @@
   @import "../../assets/styles/vars";
 
   .store-card {
-    display: inline-block;
+    display: flex;
+    flex-direction: column;
     overflow: hidden;
+
     &--wide {
       grid-column-end: span 2;
     }
+
     &__image-link {
+      display: flex;
       position: relative;
-      width: 100%;
+      height: 100%;
       overflow: hidden;
-      img {
-        width: 100%;
-        height: calc(100% - 2.9rem);
-        object-fit: fill;
-      }
+
       &:hover {
         .regular-sans-text.store-card__add-to-bag {
           background: rgba(white, 0.6);
@@ -100,6 +102,13 @@
         }
       }
     }
+
+    &__image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
     .regular-sans-text.store-card__add-to-bag {
       position: absolute;
       width: 100%;
@@ -114,21 +123,25 @@
       margin: 0;
       background: rgba(white, 0);
       color: transparent;
+
       &:hover {
         background: rgba($dark-blue, 1);
         color: white;
       }
     }
+
     &__text-wrapper {
       display: flex;
       justify-content: space-between;
       margin: 0.5rem 0;
     }
+
     &__text-link {
       display: flex;
       text-decoration: none;
       overflow: hidden;
     }
+
     .small-regular-heading.store-card__model, .regular-sans-text.store-card__type, .regular-sans-text.store-card__price {
       margin: 0;
       align-self: flex-end;
@@ -153,6 +166,7 @@
         font-size: 12px;
       }
     }
+
     .clean-button.store-card__color-button {
       width: 1rem;
       height: 1rem;
